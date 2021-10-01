1eb4741ec4cf88fc1effbdeb8be67be8a3a6c93f
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2021-10-01 17:21:54 -0500

    Refactor Build => Supported Indexer Wiki Script (#470)
    * Refactor Build => Supported Indexer Wiki Script
    
    Fixes #331 
    
    Co-authored-by: Robin Dadswell <19610103+RobinDadswell@users.noreply.github.com>

--- Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1
+++ Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1
@@ -1,108 +1,150 @@
-#Requires -Module FormatMarkdownTable
+﻿#Requires -Module FormatMarkdownTable -Version 7.1
 
 <#
     .SYNOPSIS
         Name: Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1
         The purpose of this script is to export a markdown table for the wiki of the available indexers
-
     .DESCRIPTION
-        Grabs available indexers from a locally installed Prowlarr instance
-
+        Grabs build number and available indexers from a local or remotely installed Prowlarr instance. Requires Commit Hash and App API Key
     .NOTES
-        This script has been tested on Windows PowerShell 5.1 and PowerShell 7.1.3
-
+        This script has been tested on Windows PowerShell 7.1.3
     .EXAMPLE
-    PS> .\Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1 -Commit 1.1.1.1 -Build "test" -APIKey "asjdhfjashdf89787asdfsad87676" -BaseURL http://prowlarr:9696 -OutputFile "supported-indexers.md"
-    
+    PS> .\Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1 -Commit 1.1.1.1 -Build "test" -AppAPIKey "asjdhfjashdf89787asdfsad87676" -AppBaseURL http://prowlarr:9696 -OutputFile "supported-indexers.md"
     .EXAMPLE
-    PS> .\Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1 -Commit 1.1.1.1 -Build "test" -APIKey "asjdhfjashdf89787asdfsad87676" -BaseURL http://prowlarr:9696
-
+    PS> .\Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1 -Commit 1.1.1.1 -Build "test" -AppAPIKey "asjdhfjashdf89787asdfsad87676" -AppBaseURL http://prowlarr:9696
     .EXAMPLE
-    PS> .\Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1 -Commit 1.1.1.1 -Build "test" -APIKey "asjdhfjashdf89787asdfsad87676" -OutputFile "supported-indexers.md"
-
+    PS> .\Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1 -Commit 1.1.1.1 -Build "test" -AppAPIKey "asjdhfjashdf89787asdfsad87676" -OutputFile "supported-indexers.md"
     .EXAMPLE
-    PS> .\Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1 -Commit 1.1.1.1 -Build "test" -APIKey "asjdhfjashdf89787asdfsad87676"
+    PS> .\Convert-ProwlarrSupportedIndexersToMarkdownTable.ps1 -Commit 1.1.1.1 -AppAPIKey "asjdhfjashdf89787asdfsad87676"
 #>
 
+[CmdletBinding()]
 param (
-    [Parameter(
-        Mandatory, 
-        Position = 1
-    )]
-    [string]
-    $Commit,
-    [Parameter(
-        Mandatory, 
-        Position = 2
-    )]
-    [string]
-    $Build,
-    [Parameter(
-        Mandatory,
-        Position = 3
-    )]
-    [string]
-    $APIKey,
-    [Parameter(
-        Position = 4
-    )]
-    [System.IO.FileInfo]
-    $OutputFile = ".$([System.IO.Path]::DirectorySeparatorChar)supported-indexers.md",
-    [Parameter(
-        Position = 5
-    )]
-    [uri]
-    $BaseURL = "http://localhost:9696"
-)
+    [Parameter(Mandatory, Position = 1)]
+    [string]$Commit,
+    [Parameter(Position = 2)]
+    [string]$Build,
+    [Parameter(Mandatory, Position = 3)]
+    [string]$AppAPIKey,
+    [Parameter(Position = 4)]
+    [System.IO.FileInfo]$OutputFile = ".$([System.IO.Path]::DirectorySeparatorChar)supported-indexers.md",
+    [Parameter(Position = 5)]
+    [uri]$AppBaseURL = 'http://localhost:9696'
+),
 
-$url=($BaseURL.ToString().TrimEnd("/"))+'/api/v1/Indexer/schema'
-$headers=@{'X-Api-Key'=$APIKey}
-$R= Invoke-WebRequest -Uri $url -Headers $headers -ContentType 'application/json' -Method Get
-$exp_indexername= { IF ($_.IndexerUrls) {'['+$_.name+']('+($_.IndexerUrls[0])+')'+'{#'+$_.infoLink.Replace('https://wiki.servarr.com/prowlarr/supported-indexers#','')+'}'} Else {$_.name+'{#'+$_.infoLink.Replace('https://wiki.servarr.com/prowlarr/supported-indexers#','')+'}'}}
-$indexerobject= $R.Content | ConvertFrom-Json
-$tblobj= $indexerobject | Sort-Object -Property 'name'
+# Gather Inputs & Variables
+## User Inputs
+### Convert Params to match vars
+$app_baseUrl = $AppBaseURL
+$app_apikey = $AppAPIKey
+## Start Variables
+### Application Details
+$app_api_version = 'v1'
+$app_api_path = '/api/'
+$app_api_endpoint_version = '/system/status'
+$app_api_endpoint_indexer = '/indexer/schema'
+$headers = @{'X-Api-Key' = $app_apikey }
+### Github App Info
+$gh_app_org = 'Prowlarr'
+$gh_app_repo = 'Prowlarr'
+### Wiki Details
+$wiki_link = 'https://wiki.servarr.com'
+$wiki_app_path = '/prowlarr'
+$wiki_page = 'supported-indexers'
+$wiki_bookmark = '#'
+### Page Formating
+$markdown_escape_regex = '(\w)(\.|\[|\])(\w)'
+$markdown_escape_regex_rep = '$1\$2$3'
+$wiki_1newline = "`r`n"
+$wiki_2newline = "`r`n`r`n"
+$wiki_encoding = 'utf8'
+### Github Details
+$gh_web = 'https://github.com'
+$gh_web_commit = 'commit/'
+## End Variables
 
-#$tbl_PubUse= $tblobj | Where-Object {($_.privacy -eq "public") -and ($_.protocol -eq "usenet")} | `
-#         Select-Object @{Name = 'Indexer'; Expression =$exp_indexername}, `
-#         @{Name = 'Language'; Expression = {$_.language}}, `
-#         @{Name = 'Description'; Expression = {$_.description}}
+Write-Information 'Variables and Inputs Imported'
+## Build Parameters
+### App
+$api_url = ($app_baseUrl.ToString().TrimEnd('/')) + $app_api_path + $app_api_version
+$version_url = $api_url + $app_api_endpoint_version
+$indexer_url = $api_url + $app_api_endpoint_indexer
+### Github
+$gh_repo_org = $gh_app_org + '/' + $gh_app_repo + '/'
+### Wiki
+$wiki_infolink = ($wiki_link.ToString().TrimEnd('/')) + $wiki_app_path + '/' + $wiki_page + $wiki_bookmark
+$wiki_commiturl = ($gh_web.ToString().TrimEnd('/')) + '/' + $gh_repo_org + $gh_web_commit
+Write-Information 'Parameters Built'
 
-$tbl_PrvUse= $tblobj | Where-Object {($_.privacy -CIn "private" -and $_.protocol -eq "usenet")} | `
-         Select-Object @{Name = 'Indexer'; Expression =$exp_indexername}, `
-         @{Name = 'Language'; Expression = {$_.language}}, `
-         @{Name = 'Description'; Expression = {$_.description}}
+## Invoke Requests & Convert to Objects
+Write-Information 'Getting Version Data and Converting Response to Object'
+$version_obj = (Invoke-WebRequest -Uri $version_url -Headers $headers -ContentType 'application/json' -Method Get).Content | ConvertFrom-Json
+Write-Information 'Got App Version'
+Write-Information 'Getting Indexer Data and Converting Response to Object'
+$indexer_obj = (Invoke-WebRequest -Uri $indexer_url -Headers $headers -ContentType 'application/json' -Method Get).Content | ConvertFrom-Json
+Write-Information 'Got Indexer Data'
+## We use the regex to escape dots (URLS) and Brackets (markdown links) in names and descriptions
+$indexer_name_exp = { IF ($_.IndexerUrls) { '[' + ($_.name -replace $markdown_escape_regex, $markdown_escape_regex_rep) + '](' + ($_.IndexerUrls[0]) + ')' + '{#' + $_.infoLink.Replace($wiki_infolink.ToString(), '') + '}' } Else { ($_.name -replace $markdown_escape_regex, $markdown_escape_regex_rep) + '{#' + $_.infoLink.Replace($wiki_infolink.ToString(), '') + '}' } }
+$usenet_indexer_name_exp = { IF ($_.IndexerUrls) { '[' + ($_.name -replace $markdown_escape_regex, $markdown_escape_regex_rep) + '](' + ($_.IndexerUrls[0]) + ')' + '{#' + $_.infoLink.Replace($wiki_infolink.ToString(), '') + '}' } Else { IF ($_.fields.value[0]) { '[' + ($_.name -replace $markdown_escape_regex, $markdown_escape_regex_rep) + '](' + ($_.fields.value[0].Replace('api.', '').Replace('feed.', '')) + ')' + '{#' + $_.infoLink.Replace($wiki_infolink.ToString(), '') + '}' } Else { ($_.name -replace $markdown_escape_regex, $markdown_escape_regex_rep) + '{#' + $_.infoLink.Replace($wiki_infolink.ToString(), '') + '}' } } }
+$indexer_description_exp = { ($_.description -replace $markdown_escape_regex, $markdown_escape_regex_rep) }
 
-$tbl_PubTor= $tblobj | Where-Object {($_.privacy -eq "public") -and ($_.protocol -eq "torrent")} | `
-         Select-Object @{Name = 'Indexer'; Expression =$exp_indexername}, `
-         @{Name = 'Language'; Expression = {$_.language}}, `
-         @{Name = 'Description'; Expression = {$_.description}}
-        
-        
-$tbl_PrvTor= $tblobj | Where-Object {($_.privacy -CIn "private" -and $_.protocol -eq "torrent")} | `
-         Select-Object @{Name = 'Indexer'; Expression =$exp_indexername}, `
-         @{Name = 'Language'; Expression = {$_.language}}, `
-         @{Name = 'Description'; Expression = {$_.description}}         
+## Determine Commit
+Write-Information "Commit is $commit"
 
-#$fmttablePubUse= ConvertTo-Markdown($tbl_PubUse)
-$fmttablePubUse= "None"
-$fmttablePrvUse= $tbl_PrvUse | Format-MarkdownTableTableStyle Indexer, Description, Language -HideStandardOutput -ShowMarkdown -DoNotCopyToClipboard
-$fmttablePubTor= $tbl_PubTor | Format-MarkdownTableTableStyle Indexer, Description, Language -HideStandardOutput -ShowMarkdown -DoNotCopyToClipboard 
-$fmttablePrvTor= $tbl_PrvTor | Format-MarkdownTableTableStyle Indexer, Description, Language -HideStandardOutput -ShowMarkdown -DoNotCopyToClipboard
+## Determine Version (Build)
+Write-Information 'Determining Build'
+$build = $version_obj | Select-Object -ExpandProperty 'version' | Out-String | ForEach-Object { $_ -replace "`n|`r", '' }
+Write-Information "Build is $build"
 
-$prefix= "`n"+"- Supported Indexers as of Build ``" + $build + "`` / [Commit: " + $commit + "](https://github.com/Prowlarr/Prowlarr/commit/" + $commit + ")"
+Write-Information 'Ingesting Indexer Data'
+## Get Indexer Data
+$indexer_tbl_obj = $indexer_obj | Sort-Object -Property 'name'
 
-$fmttabletor= "`r`n"+"## Torrents" + "`r`n`r`n"  + "### Public" + "`r`n`r`n" + $fmttablePubTor + "`r`n`r`n" + "### Private" + "`r`n`r`n" + $fmttablePrvTor + "`r`n`r`n"
-$fmttableuse= "## Usenet" + "`r`n`r`n" + "### Public" + "`r`n`r`n" + $fmttablePubUse + "`r`n`r`n" + "### Private" + "`r`n`r`n" + $fmttablePrvUse
-$date = [DateTime]::UtcNow.ToString("o")
-$mdHeader = "---
+## Build Table Fields
+Write-Information 'Building Indexer Tables'
+### Public Usenet
+Write-Information 'Building: Usenet - Public'
+$tbl_PubUse = $indexer_tbl_obj | Where-Object { ($_.privacy -eq 'public') -and ($_.protocol -eq 'usenet') } | Select-Object @{Name = 'Indexer'; Expression = $usenet_indexer_name_exp }, @{Name = 'Language'; Expression = { $_.language } }, @{Name = 'Description'; Expression = $indexer_description_exp }
+### Private Usenet
+Write-Information 'Building: Usenet - Private'
+$tbl_PrvUse = $indexer_tbl_obj | Where-Object { ($_.privacy -CIn 'private' -and $_.protocol -eq 'usenet') } | Select-Object @{Name = 'Indexer'; Expression = $usenet_indexer_name_exp }, @{Name = 'Language'; Expression = { $_.language } }, @{Name = 'Description'; Expression = $indexer_description_exp }
+### Public Torrents
+Write-Information 'Building: Torrents - Public'
+$tbl_PubTor = $indexer_tbl_obj | Where-Object { ($_.privacy -eq 'public') -and ($_.protocol -eq 'torrent') } | Select-Object @{Name = 'Indexer'; Expression = $indexer_name_exp }, @{Name = 'Language'; Expression = { $_.language } }, @{Name = 'Description'; Expression = $indexer_description_exp }
+### Private Torrents
+Write-Information 'Building: Torrents - Private'
+$tbl_PrvTor = $indexer_tbl_obj | Where-Object { ($_.privacy -CIn 'private' -and $_.protocol -eq 'torrent') } | Select-Object @{Name = 'Indexer'; Expression = $indexer_name_exp }, @{Name = 'Language'; Expression = { $_.language } }, @{Name = 'Description'; Expression = $indexer_description_exp }
+
+## Convert Data to Markdown Table
+$tbl_fmt_PubUse = $tbl_PubUse | Format-MarkdownTableTableStyle Indexer, Description, Language -HideStandardOutput -ShowMarkdown -DoNotCopyToClipboard
+$tbl_fmt_PrvUse = $tbl_PrvUse | Format-MarkdownTableTableStyle Indexer, Description, Language -HideStandardOutput -ShowMarkdown -DoNotCopyToClipboard
+$tbl_fmt_PubTor = $tbl_PubTor | Format-MarkdownTableTableStyle Indexer, Description, Language -HideStandardOutput -ShowMarkdown -DoNotCopyToClipboard 
+$tbl_fmt_PrvTor = $tbl_PrvTor | Format-MarkdownTableTableStyle Indexer, Description, Language -HideStandardOutput -ShowMarkdown -DoNotCopyToClipboard
+Write-Information 'Builds Converted to Markdown Tables'
+
+## Page Header Info
+$wiki_page_start = $wiki_1newline + "- Supported Indexers as of Build ``" + $build + "`` / [Commit: " + $commit + '](' + $wiki_commiturl + $commit + ')'
+Write-Information 'Page Header Built'
+
+## Build Page Pieces'
+$tbl_fmt_tor = $wiki_1newline + '## Torrents' + $wiki_2newline + '### Public Trackers' + $wiki_2newline + $tbl_fmt_PubTor + $wiki_1newline + '### Private Trackers' + $wiki_2newline + $tbl_fmt_PrvTor
+$tbl_fmt_use = $wiki_1newline + '## Usenet' + $wiki_2newline + '### Public Indexers' + $wiki_2newline + $tbl_fmt_PubUse + $wiki_1newline + '### Private Indexers' + $wiki_2newline + $tbl_fmt_PrvUse
+Write-Information 'Wiki Markdown Tables Built'
+$date = [DateTime]::UtcNow.ToString('o')
+$mdHeader = 
+"`r`n---
 title: Prowlarr Supported Indexers
-description: Indexers currently named as supported in the current nightly build of Prowlarr. Other indexers are available via either Generic Newznab or Generic Torznab. 
+description: Indexers currently named as supported in the current nightly build of Prowlarr. Other indexers are available via either Generic Newznab or Generic Torznab.
 published: true
 date: $date
 tags: prowlarr, indexers
 editor: markdown
 dateCreated: $date
 ---"
-
-$fmttableall= $mdHeader + "`n" + $prefix + "`n" + $fmttabletor + $fmttableuse
-$fmttableall | Out-File $OutputFile
+Write-Information 'Wiki Page pieces built'
+## Build and Output Page
+## We replace because converting to markdown escaped the `\` as `\\` and thus `\\\\` is `\\` in file (due to regex)
+$wiki_page_file = ((($mdHeader + $wiki_1newline + $wiki_page_start + $wiki_1newline + $tbl_fmt_tor + $tbl_fmt_use) -replace '\\\\', '\') -replace '---', '---').Trim()
+Write-Information 'Wiki Page Built'
+$wiki_page_file | Out-File $OutputFile -Encoding $wiki_encoding
+Write-Information 'Wiki Page Output'
